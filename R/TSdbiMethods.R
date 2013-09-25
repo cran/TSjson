080f10305585111632043bd613d4fc77411b97d2
Paul Gilbert <pgilbert.ttv9z@ncf.ca>
2013-09-25 00:00:00 +0000

    version 2013.9-1

---
+++ R/TSdbiMethods.R
@@ -0,0 +1,237 @@
+
+setClass("jsonDriver", representation("DBIDriver", Id = "character")) 
+
+json <- function() {
+  drv <- "json"
+  attr(drv, "package") <- "TSjson"
+  new("jsonDriver", Id = drv)
+  }
+
+# require("DBI") for this
+setClass("TSjsonConnection", contains=c("DBIConnection", "conType","TSdb"),
+   representation(user="character", password="character", host="character",
+                  url="character", proxy="logical") )
+
+####### some kludges to make this look like DBI. ######
+# this does nothing but prevent errors if it is called. 
+setMethod("dbDisconnect", signature(conn="TSjsonConnection"), 
+     definition=function(conn,...) TRUE)
+#######     end kludges   ######
+
+setMethod("TSconnect",   signature(drv="jsonDriver", dbname="character"),
+  definition= function(drv, dbname, user=NULL, password=NULL, host=NULL, ...){
+ if (is.null(dbname)) stop("dbname must be specified")
+ # if other values are not specified get defaults from file or system variables
+
+ if (dbname == "proxy-cansim") {
+   dbname <- "scapi/default/get.json"
+   
+   f <- paste(Sys.getenv("HOME"),"/.TSjson.cfg", sep="")
+   if (file.exists(f)) {
+       f <- scan(f, what="") # parse a file for [proxy-cansim] user password host
+       # only proxy-cansim supported for now
+       r <- list(user=f[2],        
+                 password = f[3] , 
+                 host     = f[4]   
+		 )
+      }
+   else  r <- list(user     = Sys.getenv()["TSJSONUSER"],
+                   password = Sys.getenv()["TSJSONPASSWORD"],
+                   host     = Sys.getenv()["TSJSONHOST"])
+    
+   if (is.null(user)) user <- r$user
+   if (is.null(password)) password <-r$password
+   if (is.null(host)) host <- r$host
+   url <- paste("http://",user,":",password,"@",host,"/",dbname,"/", sep="")
+   proxy <- TRUE
+   }
+ else if (dbname == "cansim") {
+   user <- password <- host  <- ""
+   # this is not really a url in this case, but the .py has the url+
+   # shQuote() for path name spaces in Windows
+   url <- shQuote(paste(path.package("TSjson"), "/exec/cansimGet.py", sep=""))
+   # specifying python is for Windows. Script works with or without in linux
+   url <- paste("python ",url) 
+   proxy <- FALSE
+   }
+ else stop("dbname ", dbname, " not supported.")
+
+ # there could be a better connection test mechanism 
+ #if(inherits(con, "try-error")) 
+ #      stop("Could not establish TSjsonConnection to ",  dbname)
+   
+ new("TSjsonConnection", drv="json", dbname=dbname,
+        hasVintages=FALSE, hasPanels=FALSE, 
+	user=user, password=password, host=host, url=url, proxy=proxy ) 
+ } )
+
+
+setMethod("TSdates",
+  signature(serIDs="character", con="TSjsonConnection", vintage="ANY", panel="ANY"),
+   definition= function(serIDs, con, vintage=NULL, panel=NULL, ... )  
+{  # Indicate  dates for which data is available.
+   # This requires retrieving series individually so they are not truncated.
+   r <- av <- st <- en <- tb <- NULL
+   for (i in 1:length(serIDs))
+     {r <- try(TSget(serIDs[i], con, quiet=TRUE), silent = TRUE)
+
+      if(inherits(r, "try-error") ) {
+        av <- c(av, FALSE)
+	st <- append(st, list(NA))
+	en <- append(en, list(NA))
+	tb <- rbind(tb, NA)
+	}
+      else  {
+        av <- c(av, TRUE)
+        st <- append(st, list(tfstart(r)))
+        en <- append(en, list(tfend(r)))
+        tb <- rbind(tb,tffrequency(r))
+        }
+      }
+  r <- serIDs
+  attr(r, "TSdates") <- av
+  attr(r, "start") <- st
+  attr(r, "end")   <- en
+  attr(r, "frequency")   <- tb
+  class(r) <- "TSdates"
+  r
+} )
+
+
+setMethod("TSget",     signature(serIDs="character", con="TSjsonConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"), 
+       TSrepresentation=getOption("TSrepresentation"),
+       tf=NULL, start=tfstart(tf), end=tfend(tf),
+       names=serIDs, 
+       TSdescription=FALSE, TSdoc=FALSE, TSlabel=FALSE, TSsource=TRUE,
+       quiet=TRUE, repeat.try=3, ...){ 
+       
+  if(is.null(TSrepresentation)) TSrepresentation <- "default"
+  if(is.null(repeat.try)) repeat.try <- 5
+  
+  url <- con@url 
+
+  mat <- desc <- doc <- label <- source <-  rp <- NULL
+
+  for (i in seq(length(serIDs))) {
+    qq <- paste(url, serIDs[i], sep=" ")
+    
+    if(con@proxy){
+       for (rpt in seq(repeat.try)) {
+   	      rr <- try(getURL(qq), silent=quiet)
+   	      if (!inherits(rr , "try-error")) break
+   	      }
+
+       if(inherits(rr , "try-error") ) stop(# after repeating
+ 	    "Series retrieval failed. Server ", con@host, "not responding.")
+
+       # there may also be attr(rr,"errmsg") available
+       if ((!is.null(attr(rr,"status"))) && (0 !=  attr(rr,"status")) ) 
+   	  stop("Series retrieval failed. Series ",serIDs[i], " may not exist.")
+
+       rr <-  try(fromJSON(rr, asText=TRUE), silent=quiet)
+       if(inherits(rr , "try-error") ) stop(
+   	   "Conversion from JSON failed, server returning unrecognized object.")
+       } 
+    else {#!con@proxy
+       for (rpt in seq(repeat.try)) {
+	    #rr <- try(system(qq, intern=TRUE), silent=quiet)
+   	    #fromJSON in RJSONIO (requires change of Imports: and NAMESPACE):
+	    #rr <- fromJSON(pcon <- pipe(qq), asText=TRUE)
+  	    #fromJSON in rjson:
+	    rr <- fromJSON(readLines(pcon <- pipe(qq)))
+   	    close(pcon)
+	    if ((!inherits(rr , "try-error"))){
+	       if(is.atomic(rr)) stop(rr, "\n rr is atomic. DEBUG py.")
+	       else if(is.null(rr$error)) break
+	       }
+   	    }
+       if(inherits(rr , "try-error")) # after repeating
+   	  stop("system command or fromJSON did not execute properly.")
+       else if(!is.null(rr$error)) stop("error retrieving series: ", rr$error)
+       
+       # this is for system() rather than pipe()
+       # if ((!is.null(attr(rr,"status"))) && (0 !=  attr(rr,"status")) ) stop( 
+       #   "Series retrieval failed. Series ",serIDs[i], " may not exist.")
+       }
+
+    if(0==length(rr))
+       stop("Series retrieval failed. Series ",serIDs[i], " may not exist.")
+
+    fr <- rr$freq
+    if("Error" == fr) stop("frequency not recognized.")
+    st <- rr$start
+    x  <- rr$x
+    
+    #this is necessary sometimes. unlist(x) would be ok but missing
+    # values (py None are translated to json null and then as null
+    # in the R list) get truncated out with unlist(x)
+    if(is.list(x)) {
+        #warning("need to unlist fromJSON x result.")
+	na <- unlist(lapply(x, is.null))
+	z <- unlist(x)
+	x <- rep(NA, length(na)) 
+	x[!na] <- z 
+	}
+    
+    if((TSrepresentation=="default" | TSrepresentation=="ts") 
+           && fr %in% c(1,4,12,2))
+	 r <-   ts(x, start=st, frequency=fr) 
+    else {
+         require("tframePlus")
+	 require("zoo")
+	 r <-  zoo(x, order.by=as.Date(rr$dates, format='%b %d %Y'))
+	 }
+
+    mat <- tbind(mat, r)
+    if(TSdescription) desc <- c(desc,   rr$shortdesc ) 
+    if(TSdoc)     doc      <- c(doc,    rr$desc ) 
+    if(TSlabel)   label    <- c(label,  serIDs[i] )
+    if(TSsource)  source   <- c(source, rr$source )
+    }
+
+  if (NCOL(mat) != length(serIDs)) stop("Error retrieving series", serIDs) 
+
+  mat <- tfwindow(mat, tf=tf, start=start, end=end)
+
+  if( (!is.null(rp)) && !all(is.na(rp)) ) TSrefperiod(mat) <- rp      
+
+  if (! TSrepresentation  %in% c( "zoo", "default")){
+      require("tframePlus")
+      mat <- changeTSrepresentation(mat, TSrepresentation)
+      }
+
+  seriesNames(mat) <- names 
+
+  TSmeta(mat) <- new("TSmeta", serIDs=serIDs,  
+      hasVintages=con@hasVintages, hasPanels=con@hasPanels,
+      conType=class(con), 
+      DateStamp=Sys.time(), 
+      TSdescription = if(TSdescription) desc   else NA, 
+      TSdoc         = if(TSdoc)         doc    else NA,
+      TSlabel       = if(TSlabel)       label  else NA,
+      TSsource      = if(TSsource)      source else NA )
+  mat
+} )
+
+
+#setMethod("TSput",     signature(x="ANY", serIDs="character", con="TSjsonConnection"),
+#   definition= function(x, serIDs=seriesNames(data), con, ...)   
+#    "TSput for TSjson connection not supported." )
+
+setMethod("TSdescription",   signature(x="character", con="TSjsonConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...){
+        TSdescription(TSget(serIDs=x, con=con, TSdescription=TRUE ))})
+
+setMethod("TSdoc",   signature(x="character", con="TSjsonConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...){
+        TSdoc(TSget(serIDs=x, con=con, TSdoc=TRUE ))})
+
+setMethod("TSlabel",   signature(x="character", con="TSjsonConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...){
+        TSlabel(TSget(serIDs=x, con=con, TSlabel=TRUE ))})
+
+setMethod("TSsource",   signature(x="character", con="TSjsonConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...){
+        TSsource(TSget(serIDs=x, con=con, TSsource=TRUE ))})
+
---
+++ exec/cansimGet.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# eg
+#    cansimGet.py v498086
+#    ./cansimGet.py V36610
+#    ./cansimGet.py V39050
+# or
+#    python  cansimGet.py v498086
+
+# weekly
+#    cansimGet.py V36610
+
+# based on code by Geoff Wright
+
+import sys
+import json
+
+mnem = sys.argv[1]
+
+def get(mnem):
+    
+    if sys.version_info >= (3, 0): return dict( error=
+	"TSjson cansimGet.py require Python 2. Running "+ str(sys.version_info))
+    # mechanize is not (yet) available for Python 3, 
+    # Also, urllib2 is split into urllib.request, urllib.error in Python 3
+    import urllib2
+    import mechanize, re, csv
+    # Open search form
+    response = mechanize.urlopen("http://www5.statcan.gc.ca/cansim/home-accueil?lang=eng")
+    if not response: return dict(error="URL not responding.", mnem=mnem)
+
+    #mnem=request.args[0]
+    # Find form
+    forms = mechanize.ParseResponse(response, backwards_compat=False)
+    form = forms[0]
+    form.set_value(mnem, name="pattern", kind="text")
+    request2 = form.click()
+
+    response2 = mechanize.urlopen(request2)
+    if not response2: 
+        return dict(error="Form navigation step 2 failed.", mnem=mnem)
+    forms = mechanize.ParseResponse(response2, backwards_compat=False)
+    form = forms[0]
+    try: 
+        request3 = form.click(id="a08Sbm")
+    except: 
+        return dict(error="Series not found.", mnem=mnem)
+
+    response3 = mechanize.urlopen(request3)
+    if not response3:
+        return dict(error="Form navigation step 3 failed.", mnem=mnem)
+    forms = mechanize.ParseResponse(response3, backwards_compat=False)
+    form = forms[0]
+    
+    try: 
+        control = form.find_control("smonth", type="select")
+        startper = str(control.get_items()[0]).lstrip("*")
+        form.set_value([startper],name="smonth")
+    except: 
+        startper = 12
+        
+    control = form.find_control("syear", type="select")
+    startyear = str(control.get_items()[0]).lstrip("*")
+
+    form.set_value([startyear],name="syear")
+    form.set_value(['SERIES_CSV_TIME_AS_ROW'],name="exporterId")
+    request4 = form.click(label="Retrieve now")
+    
+    response4 = mechanize.urlopen(request4)
+    if not response4:
+        return dict(error="Form navigation step 4 failed.", mnem=mnem)
+    
+    csv_search = re.search('http://www5.statcan.gc.ca/cansim/results/cansim.[0-9]*\.csv', response4.read())
+    if not csv_search:
+        return dict(error="File to download not found.", mnem=mnem)
+    
+    f_url = csv_search.group()
+    
+    f = urllib2.urlopen(f_url)
+    if not f: return dict(error="Retrieving file failed.", mnem=mnem)
+   
+    a = []
+    for l in f.readlines():
+        a.append(l)
+    
+    try: desc = a[1].split('"')[1].strip("\n")
+    except:desc=a[1] 
+    shortdesc = desc.split('; ')[-1]
+    freq = a[2].split(',')[0].strip("\n")
+    mnem = a[2].split(',')[1].strip("\r\n")
+    source = "Statistics Canada: "+desc.split(":")[0]+"; "+mnem
+    
+    # Format to make accessible as R object
+    if freq == "Annual":
+        freq = 1
+        startper = 1
+    elif freq == "Semi-annual":
+        freq = 2
+        startper = 1
+    elif freq == "Quarterly":
+        freq = 4
+        startper = int(startper)/3
+    elif freq == "Monthly":
+        freq = 12
+        startper = int(startper)
+    elif freq == "Weekly":
+        startper = 1
+    elif freq == "Daily":
+        startper = 1
+    else:
+        freq = "Error"
+    
+    start = [int(startyear),startper]
+    
+    d = []
+    x = []
+    for i in range(3,len(a)):
+       xi = a[i]
+       if (freq == "Weekly") | (freq == "Daily"):
+            d.append(xi.split(',')[0])
+       
+       xi = xi.split(',')[1].strip("\r\n")
+       
+       if xi == "":
+            x.append(None)
+       elif xi == "..":
+            x.append(None)
+	    # check this bad data point with statcan or BoC
+       else:
+            x.append(float(xi))
+    
+    #session.shortdesc = shortdesc
+    #session.x = x
+    
+    if (freq == "Weekly") | (freq == "Daily"):
+       return dict(desc=desc,shortdesc=shortdesc,freq=freq,
+                mnem=mnem,dates=d,x=x,source=source)
+    else:
+       return dict(desc=desc,shortdesc=shortdesc,freq=freq,
+                mnem=mnem,start=start,x=x,source=source)
+
+
+#z = get(mnem)
+#print '%(mnem)s has freq %(freq)03d ' % z
+#print 'dates %(dates)s ' % z
+
+print(json.JSONEncoder().encode(get(mnem)))
